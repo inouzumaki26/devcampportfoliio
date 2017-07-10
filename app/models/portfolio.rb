37b3f16ed4f9b6ed9807809e4a2ff898dc161953
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 14:05:15 +0000

    Integrated database relationship between topics and blog posts.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,18 @@
 class Portfolio < ApplicationRecord
-  validate_presence_of :title, :body, :main_image, :thumb_image
+  has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda {|attrs| attrs['name'].blank}
+   
+  validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+  
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.main_image ||= Placeholer.image_generator(height: '600', width: '400')
+        self.thumb_image ||= Placeholer.image_generator(height: '350', width: '200')
+  end
 end
