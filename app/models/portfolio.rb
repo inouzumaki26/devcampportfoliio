7d1927f95d94f2d86103e0733303ba33da92e324
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:44:23 +0000

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,4 @@
 class Portfolio < ApplicationRecord
-  has_many :technologies
-  accepts_nested_attributes_for :technologies,
-                                reject_if: lambda {|attrs| attrs['name'].blank}
-   
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   def self.angular
@@ -11,8 +7,10 @@ class Portfolio < ApplicationRecord
   
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
   
+  after_initialize :set_defaults
+  
   def set_defaults
-    self.main_image ||= Placeholer.image_generator(height: '600', width: '400')
-        self.thumb_image ||= Placeholer.image_generator(height: '350', width: '200')
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
   end
 end
