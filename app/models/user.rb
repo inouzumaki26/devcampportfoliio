5517f48c6093f0e39859843e7f0b7da3aa222e36
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 22:26:30 +0000

    Implemented bootstrap gem and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
- validates_presence_of :name
+  validates_presence_of :name
  
   def first_name
-    self.name.split.first
+    begin
+      self.name.split.first.oops
+    rescue
+    end
   end
   
   def last_name
     self.name.split.last
   end
-  
+end
+
+
+def get_facebook_massages
+  begin
+    contacts_fb.oops
+    @messages = retrieves_messages
+  rescue => e
+    flash[:error] = "Error occured contacting Facebook: #{e}"
+  end
 end
