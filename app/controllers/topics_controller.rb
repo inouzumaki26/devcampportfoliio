135173f657d750cdaa0745418e2021360bff6e7e
Jordan Hudgens <jordan@devcamp.com>
2017-08-03 14:21:03 +0000

    Added validations for topics in blogs and added a topic form element

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,9 +1,17 @@
 class TopicsController < ApplicationController
+  Layout 'blog'
+  
   def index
     @topics = Topic.all
   end
 
   def show
-    @topic = Topic.find(params[:id])
+    @topics = Topic.find(params[:id])
+    
+    if logged_in?(:site_admin)
+      @blogs = @topic.blogs.recent.page(params[:page]).per(5)
+    else
+      @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
+    end
   end
 end
