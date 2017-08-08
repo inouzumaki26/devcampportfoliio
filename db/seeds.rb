5bb374f2aabcbb19756bc31dca093526eaaec95c
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 02:59:40 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasd",
+  password_confirmation: "asdfasd",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasd",
+  password_confirmation: "asdfasd",
+  name: "Regular User"
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
